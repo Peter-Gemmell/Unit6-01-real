6b7384bbcc81d2391bc6261b1217d8f3080a5c3c
Peter Gemmell <peter.gemmell@ocsbstudent.ca>
2020-12-17 18:58:57 +0000

    cehck check

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var randomNumber = Math.floor(Math.random() * 6) + 1;
+let randomNumber = Math.floor(Math.random() * 6) + 1
 
-var guess= prompt('Pick a Number any Number out of 6. Will you guess correctly to the Rdm Number Generator?');
+let guess = prompt('Pick a Number any Number out of 6. Will you guess correctly to the Rdm Number Generator?')
 
-if (parseInt(guess) == randomNumber ) {
+if (parseInt(guess) === randomNumber) {
 
-  document.write('<p>That is Correct</p>');
+  document.write('<p>That is Correct</p>')
 
 } else {
 
-  document.write('<p> Thats Incorrect, Twas '  + randomNumber + '</p>');
+  document.write('<p> Thats Incorrect, Twas '+ randomNumber + '</p>')
 
 }
