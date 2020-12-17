e7cab1f975fb650e7b578f754af91de990eda706
Peter Gemmell <peter.gemmell@ocsbstudent.ca>
2020-12-17 19:03:19 +0000

    ?

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-let randomNumber = Math.floor(Math.random() * 6) + 1
+const randomNumber = Math.floor(Math.random() * 6) + 1
 
-let guess = prompt('Pick a Number any Number out of 6. Will you guess correctly to the Rdm Number Generator?')
+const guess = prompt('Pick a Number any Number out of 6. Will you guess correctly to the Rdm Number Generator?')
 
 if (parseInt(guess) === randomNumber) {
-
   document.write('<p>That is Correct</p>')
-
 } else {
-
-  document.write('<p> Thats Incorrect, Twas '+ randomNumber + '</p>')
-
+  document.write('<p> Thats Incorrect, Twas ' + randomNumber + '</p>')
 }
